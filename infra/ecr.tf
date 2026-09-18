1a299bb38945a0c6e6e32dd127f5646da3442826
aravinthsamys-tw <aravinthsamy.s@thoughtworks.com>
2026-09-18 11:22:11 +0800

    infra: add container registry

---
+++ infra/ecr.tf
@@ -0,0 +1,14 @@
+resource "aws_ecr_repository" "service" {
+  name                 = local.name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = local.common_tags
+}
